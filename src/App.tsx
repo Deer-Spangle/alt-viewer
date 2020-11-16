724b225a3bfcb9defcc62558bc5e8326c6bd0865
Deer-Spangle <deer@spangle.org.uk>
2020-11-16 15:09:07 +0000

    Removing react template, logo and stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import styles from './App.module.css';
 import {ViewerSelector} from "./ViewerSelector";
 
@@ -29,21 +28,6 @@ const App: React.FunctionComponent = () => {
                 foregroundImg={foregroundImg}
                 backgroundImg={backgroundImg}
             />
-            <header className={styles.AppHeader}>
-                <img src={logo} className={styles.AppLogo} alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-
-                <a
-                    className={styles.AppLink}
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
         </div>
     );
 }
