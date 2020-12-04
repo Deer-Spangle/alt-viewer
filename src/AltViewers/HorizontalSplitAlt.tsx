48a07ae933b99a805056bc6b8571d85dee5f0ea3
Deer-Spangle <deer@spangle.org.uk>
2020-12-04 16:00:35 +0000

    Replacing horizontal and vertical slider borders with an svg hint, making it more clear to drag. Also defaulting to 100px across for each slider, so that it's on the screen

--- src/AltViewers/HorizontalSplitAlt.tsx
+++ src/AltViewers/HorizontalSplitAlt.tsx
@@ -30,7 +30,7 @@ const ForegroundImg: React.FunctionComponent<ForegroundProps> = (props) => {
 
     return <div className={styles.ForegroundWrapper} ref={ref}>
         <div className={styles.ForegroundSlider} style={{
-            "height": mouse.y || 0,
+            "height": mouse.y || 100,
         }}>
             <HotlinkImage
                 src={props.foregroundImg}
@@ -38,5 +38,31 @@ const ForegroundImg: React.FunctionComponent<ForegroundProps> = (props) => {
                 alt="Foreground"
             />
         </div>
+        <HorizontalSplitHint />
+    </div>
+}
+
+const HorizontalSplitHint: React.FunctionComponent = () => {
+    return <div className={styles.SliderHint}>
+        <svg width="100%" height="100">
+            <mask id="mask">
+                <rect x="-50%" y="-50%" width="100%" height="100%" fill="white"/>
+                <rect x="-30" y="-30" width="60" height="60" fill="black"/>
+            </mask>
+            <g style={{transform:"translate(50%,50%)"}}>
+                <g mask="url(#mask)">
+                    <line x1="0" y1="-5" x2="0" y2="5" stroke="rgba(255,255,255,.9)" strokeWidth="2"/>
+                    <line x1="-50%" y1="0" x2="50%" y2="0" stroke="rgba(255,255,255,.9)" strokeWidth="4"/>
+                </g>
+                <rect x="-30" y="-30" width="60" height="60" fill="none" stroke="rgba(255,255,255,1)" strokeWidth="4"/>
+                <path
+                    d="M-10,-10 L0,-20 L10,-10 M-10,10 L0,20 L10,10"
+                    id="Path"
+                    stroke="#FFFFFF"
+                    strokeWidth="4"
+                    fill="none"
+                />
+            </g>
+        </svg>
     </div>
 }
--- src/AltViewers/VerticalSplitAlt.tsx
+++ src/AltViewers/VerticalSplitAlt.tsx
@@ -28,14 +28,42 @@ const ForegroundImg: React.FunctionComponent<ForegroundProps> = (props) => {
     const mouse = useMouseLatest(ref)
 
     return <div className={styles.ForegroundWrapper} ref={ref}>
-        <div className={styles.ForegroundSlider} style={{
-            "width": mouse.x || 0,
+        <div className={styles.ForegroundSliderWrapper} style={{
+            "width": mouse.x || 100,
         }}>
-            <HotlinkImage
-                src={props.foregroundImg}
-                className={styles.ImgForeground}
-                alt="Foreground"
-            />
+            <VerticalSplitHint />
+            <div className={styles.ForegroundSlider}>
+                <HotlinkImage
+                    src={props.foregroundImg}
+                    className={styles.ImgForeground}
+                    alt="Foreground"
+                />
+            </div>
         </div>
+    </div>
+}
+
+const VerticalSplitHint: React.FunctionComponent = () => {
+    return <div className={styles.SliderHint}>
+        <svg width="100" height="100%">
+            <mask id="mask">
+                <rect x="-50%" y="-50%" width="100%" height="100%" fill="white"/>
+                <rect x="-30" y="-30" width="60" height="60" fill="black"/>
+            </mask>
+            <g style={{transform:"translate(50%,50%)"}}>
+                <g mask="url(#mask)">
+                    <line x1="0" y1="-50%" x2="0" y2="50%" stroke="rgba(255,255,255,.9)" strokeWidth="4"/>
+                    <line x1="-5" y1="0" x2="5" y2="0" stroke="rgba(255,255,255,.9)" strokeWidth="2"/>
+                </g>
+                <rect x="-30" y="-30" width="60" height="60" fill="none" stroke="rgba(255,255,255,1)" strokeWidth="4"/>
+                <path
+                    d="M-10,-10 L-20,0 L-10,10 M10,-10 L20,0 L10,10"
+                    id="Path"
+                    stroke="#FFFFFF"
+                    strokeWidth="4"
+                    fill="none"
+                />
+            </g>
+        </svg>
     </div>
 }
